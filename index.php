2d090d3351921397a3eb70a35b7c63c8c104758a
jahcr1 <martineta_contreras@hotmail.com>
2024-10-01 12:06:58 -0300

    Agregando el .gitignore modificado y el index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require __DIR__ . 'vendor/autoload.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recetas 133</title>
+</head>
+<body>
+    <section id="inicio">
+        <div class="container">
+
+        </div>
+    </section>
+    <section id="recetas-populare"></section>
+    <section id="buscar"></section>
+</body>
+</html>
